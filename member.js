2a67d83adc9735faa0a17a5e03960a9f36203925
Anis Farhan Itsnaini <79190974+anisfarhan@users.noreply.github.com>
2025-03-09 08:54:21 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,18 @@
+function skillsMember() {
+  var skills = [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "React",
+    "Node",
+    "Express",
+    "MongoDB",
+    "Python",
+    "Django",
+    "Flask",
+    "PostgreSQL",
+    "Ruby",
+    "Rails"
+  ];
+  return skills;
+}
